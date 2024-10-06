405e9b2981f5eec30767de94aef931a8c258d663
shainasahu <145936084+shainasahu@users.noreply.github.com>
2024-10-05 23:54:14 -0400

    modified web scraping files

--- web_scraping/amazon.py
+++ web_scraping/amazon.py
@@ -10,11 +10,13 @@ def getProductData(url):
     price_el = driver.find_element(By.ID, "corePriceDisplay_desktop_feature_div")
     description_el = driver.find_element(By.ID, "title_feature_div")
     brand_el = driver.find_element(By.ID, "bylineInfo_feature_div")
+    color_el = driver.find_element(By.ID, "variation_color_name")
 
     val["product_details"] = product_details_el.text
     val["price"] = price_el.text
     val["description"] = description_el.text
     val["brand"] = brand_el.text
+    val["color"] = color_el.text
 
     driver.close()
 
