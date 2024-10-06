e6ea709ecfc06f38970077db311f3a0830359047
MNery <mfn2120@columbia.edu>
2024-10-06 02:44:28 -0400

    Revert ""
    This reverts commit 905a23e684a07fb6320c918b5e7ea82076119c65, reversing
    changes made to 1e7bbe0ca9f09e0cdf499b9afbe38b03d1e6a196.

---
+++ ecotag/lib/widgets/logo_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/EcoTag.png'); // Make sure to add the logo in assets
+  }
+}
--- ecotag/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ecotag/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import auth0_flutter
 import flutter_secure_storage_macos
 import path_provider_foundation
@@ -14,8 +13,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   Auth0FlutterPlugin.register(with: registry.registrar(forPlugin: "Auth0FlutterPlugin"))
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> 4072eb670da315986b3493ce4da27f0eff11aab1
 }
